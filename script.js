7d939d1a97f0c1c8fca1d959548edb5c4ba30c9c
Godswill Okereke <okerekegodswill7@gmail.com>
2025-10-16 23:09:12 +0100

    cleaning of codes

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-
-// script.js
-
 function updateTime() {
   const currentTime = Date.now(); // current time in milliseconds
   document.querySelector('[data-testid="test-user-time"]').textContent = currentTime;
@@ -10,5 +7,5 @@ function updateTime() {
 // update once when the page loads
 updateTime();
 
-// optional: update every second for live accuracy
+//update every second for live accuracy
 setInterval(updateTime, 1000);
